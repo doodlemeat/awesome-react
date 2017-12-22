777c5705f41c7d96d8302a320633aca20da8b0b6
Anthon Fredriksson <anthonnorrby@gmail.com>
2017-12-23 00:33:02 +0100

    server

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <table className="result-table">
+          <tr>
+            <th className="align-left">Repository</th>
+            <th className="align-left">Category</th>
+            <th className="align-left">Description</th>
+            <th>Last commit</th>
+            <th>Stars</th>
+          </tr>
+          <tr>
+            <td><a href="https://github.com/enaqx/awesome-react">https://github.com/enaqx/awesome-react</a></td>
+            <td>Styling</td>
+            <td>...</td>
+            <td className="align-center">4 days ago</td>
+            <td className="align-center">4,560</td>
+          </tr>
+        </table>
       </div>
     );
   }
